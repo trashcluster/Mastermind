fe3a1dc2449030b2072f04a719d90bd1f423ba55
trashcluster <info@trashcluster.com>
2019-03-08 15:28:11 +0100

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,16 +1,39 @@
-solution=[1,2,3,4]
-teste=[1,4,2,4]
+from random import *
 
-def mm (solution, teste):
-    #print(solution,teste)
-    resultat=[0,0,0,0]
+#fonction de création de la solution
+def fsolution (colonnes, couleurs):
+    solution=[]
+    for i in range(colonnes):
+        solution.append(int(couleurs-1*random()+1))
+    return solution
+
+#fonction d'entrée des valeurs à tester dans le tableau test
+def ftest (colonnes):
+    test=[]
+    for i in range(colonnes):
+        test.append(int(input("Entre la valeur",i)))
+    return test
+
+
+def mm (solution, test):
+    #print(solution,test)
+#initialisation d'un tableau vide
+    resultat=[]
+#ajoute les pions rouges au tebleau resultat
     for i in range(len(solution)):
-        for j in range(len(teste)):
-            #print(resultat)
-            if solution[i] == teste[j]:
-                resultat[i]='b'
-            if solution[i] == teste[i]:
-                resultat[i]='r'
-
+        if solution[i]==test[i]:
+            resultat.append('r')
+#ajouyte les pions blanc au tableau résultat
+    for i in range(len(solution)):
+        for j in range(len(test)):
+            if solution[i] == test[j] and solution[i] != test[i]:
+                resultat.append('b')
+#si le tableau de resultat ne fait pas la longueur désiré, complète le tableau avec 'v'
+    while len(resultat)<len(solution):
+        resultat.append('v')
     return resultat
-print(mm(solution,teste))
+
+colonnes = int(input("entre le nombre de colonnes : "))
+lignes =int(input("Entre le nombre maximum d'essais : "))
+couleurs = int(input("entre le nombre de couleurs : "))
+print(mm(fsolution(colonnes,couleurs),ftest(colonnes)))
