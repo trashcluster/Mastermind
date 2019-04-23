92420c224614ac58c9c42b9265d8a06441eaeb29
trashcluster <info@trashcluster.com>
2019-04-23 15:47:23 +0200

    Update Form1.vb

--- WindowsApp1/Form1.vb
+++ WindowsApp1/Form1.vb
@@ -1,3 +1,8 @@
 ﻿Public Class Form1
+    Function fsolution(Dim colonnes As Integer, Dim couleurs As Integer)
+        Dim solution() As Single
+        For i As Integer = 1 To colonnes
 
+        Next
+    End Function
 End Class
